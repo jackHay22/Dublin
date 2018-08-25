a9b6b01d7a86517eac401146ea205d1c4f00f864
jackHay22 <jjphay@gmail.com>
2018-08-25 17:34:35 -0400

    clojure version update to 1.9.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dublin "0.3.0-SNAPSHOT"
   :description "A walk through Dublin"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :main ^:skip-aot dublin.core
   :jvm-opts ["-Xdock:name=Dublin"]
   :target-path "target/%s"
