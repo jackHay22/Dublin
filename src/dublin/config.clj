81b22ea1a4c8fd7988764ad45d49e83c51f2a155
jackHay22 <jjphay@gmail.com>
2018-09-05 13:12:24 -0400

    slow down paralax effect

--- src/dublin/config.clj
+++ src/dublin/config.clj
@@ -180,8 +180,8 @@
       (OptionBinding. "Exit" false #(System/exit 0)))
     (list
 
-      (ParalaxLayer. "menu/paralax_0.png" 0 2)
-      (ParalaxLayer. "menu/paralax_1.png" 0 1.6)
-      (ParalaxLayer. "menu/paralax_2.png" 0 1.2)
-      (ParalaxLayer. "menu/paralax_3.png" 0 0.8)
-      (ParalaxLayer. "menu/paralax_4.png" 0 -0.4))))
+      (ParalaxLayer. "menu/paralax_0.png" 0 1)
+      (ParalaxLayer. "menu/paralax_1.png" 0 0.8)
+      (ParalaxLayer. "menu/paralax_2.png" 0 0.6)
+      (ParalaxLayer. "menu/paralax_3.png" 0 0.4)
+      (ParalaxLayer. "menu/paralax_4.png" 0 -0.2))))
